33b01d24541c3ff5145b105915c29a3844befbc1
UshakovDev <ushako160696@gmail.com>
2025-10-03 03:36:06 +0900

    fix(components): обновление стилей кнопки главной страницы
    - Изменены размеры кнопки
    - Обновлены отступы и размеры заголовков на главной странице
    - Исправлены стили для улучшения отображения

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-36 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
@@ -30,7 +30,7 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-6 xl:mb-10"
           >
             Специализируюсь на веб и десктоп - разработке: проектирование архитектуры, 
             backend и frontend, интеграция сервисов, работа с базами данных. 
