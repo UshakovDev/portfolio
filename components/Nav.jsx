3a00aad107ad081178dec4571968a8179003dec1
UshakovDev <ushako160696@gmail.com>
2025-10-05 03:12:58 +0900

    fix(nav): временно закомментирована секция "услуги" в навигации

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,7 +15,7 @@ import {
 export const navData = [
   { name: "home", path: "/", Icon: HiHome },
   { name: "about", path: "/about", Icon: HiUser },
-  { name: "services", path: "/services", Icon: HiRectangleGroup },
+  // { name: "services", path: "/services", Icon: HiRectangleGroup }, Мои услуги
   { name: "work", path: "/work", Icon: HiViewColumns },
   {
     name: "testimonials",
