328241492f475130a86ddc186731c72cd6f8bb53
UshakovDev <ushako160696@gmail.com>
2025-10-14 00:53:55 +0900

    docs: Пересобрал пробую исправить проблему с top-left-img.png 03

--- components/TopLeftImg.jsx
+++ components/TopLeftImg.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const TopLeftImg = () => {
+  const router = useRouter();
+  const isAbout = router?.pathname?.startsWith("/about");
+  const blendClass = isAbout ? "mix-blend-screen" : "mix-blend-color-dodge";
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] pointer-events-none select-none">
+    <div className={`absolute left-0 top-0 ${blendClass} z-10 w-[200px] xl:w-[400px] opacity-50 pointer-events-none select-none`}>
       <Image
         src={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/top-left-img.png`}
         alt="left cover bg"
         width={400}
         height={400}
-        className="opacity-50"
       />
     </div>
   );
