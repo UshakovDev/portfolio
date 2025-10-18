9d4c76dd8f0f97de2459e8e3d3d7fadcdee1ce61
UshakovDev <ushako160696@gmail.com>
2025-10-18 23:21:40 +0900

    feat: добавлено условное позиционирование

--- components/Circles.jsx
+++ components/Circles.jsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Circles = () => {
+  const router = useRouter();
+  const needsFixed = router?.pathname?.startsWith("/about") || router?.pathname?.startsWith("/privacy");
+  const posClass = needsFixed ? "fixed" : "absolute";
+  
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+    <div className={`w-[200px] xl:w-[300px] ${posClass} -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10`}>
       <Image
         src={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/circles.png`}
         alt="circles"
