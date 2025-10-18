1d678556ada55428b64ba392318376114db5e6b2
UshakovDev <ushako160696@gmail.com>
2025-10-19 00:07:33 +0900

    docs: Пересобрал, исправил баг с circles.png_1

--- components/Circles.jsx
+++ components/Circles.jsx
@@ -7,13 +7,14 @@ const Circles = () => {
   const posClass = needsFixed ? "fixed" : "absolute";
   
   return (
-    <div className={`w-[200px] xl:w-[300px] ${posClass} -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10`}>
+    <div className={`w-[200px] xl:w-[300px] ${posClass} -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10 translate-z-0 will-change-transform pointer-events-none select-none ios-blend-fix`}>
       <Image
         src={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/circles.png`}
         alt="circles"
         width={260}
         height={200}
         className="w-full h-full"
+        style={{ WebkitTransform: 'translateZ(0)', backfaceVisibility: 'hidden', WebkitBackfaceVisibility: 'hidden' }}
       />
     </div>
   );
